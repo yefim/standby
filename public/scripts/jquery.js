2936a885a0fcda823e1a421b2481267f6510d9bd
Kathleen Warner <kathleenhwarner@gmail.com>
2014-09-16 20:50:07 -0400

    jqueryadded

---
+++ public/scripts/jquery.js
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Content Filter :: MBR95</title>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<link rel="shortcut icon" href="/shared/style/images/favicon.ico" type="image/x-icon"/>
+<link rel="icon" href="/shared/style/images/favicon.ico" type="image/x-icon"/>
+<link rel="stylesheet" type="text/css" href="/shared/style/common.cgz"/>
+<link rel="stylesheet" type="text/css" href="/shared/style/entry.cgz"/>
+<link rel="stylesheet" type="text/css" href="style/webfilter.cgz"/>
+<script type="text/javascript">
+if (top != self) { top.location.replace(location); }
+</script>
+</head>
+<body><div id="page_holder">
+<div class="header">
+<div class="wrap">
+<div class="logo"></div>
+<div class="content"></div>
+</div>
+</div>
+<div class="wrap">
+<div class="top"></div>
+<div class="center">
+<table>
+<tr>
+<td class="col1">
+<div class="router_image"></div>
+</td>
+<td class="col2">
+<div class="webfilter">
+
+<h2>Content Filter Activated</h2>
+<h3>You have reached this page because you attempted to access a restricted website.</h3>
+<h3><b>ajax.googleapis.com</b> is restricted and can not be viewed.</h3>
+<br/>
+
+<p>If you would like to adjust the content filtering settings of your
+MBR95 router
+use the <a href="http://192.168.0.1/login/redir?load=ContentFilter">Content Filtering admin page</a>.</p>
+
+
+</div>
+</td>
+</tr>
+<tr>
+<td colspan="3" class="footer">
+<div class="box">
+<div class="left">
+<span class="copyright">Copyright &copy; CradlePoint Technology, Inc. 2013 All rights reserved.</span>
+</div>
+<div class="right"></div>
+</div>
+</td>
+</tr>
+</table>
+</div>
+<div class="bottom"></div>
+</div>
+</div></body>
+</html>
