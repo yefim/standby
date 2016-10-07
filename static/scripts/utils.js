afd70a82811ca9326c47f43242735cea4a9ba4e7
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-10-06 22:18:51 -0700

    Convert relative urls to absolute urls

--- static/scripts/utils.js
+++ static/scripts/utils.js
@@ -23,31 +23,44 @@ export function crawl(urls, callback) {
     pool.releaseWorker(crawler);
 
     if (e.data.success) {
-      console.log(e.data);
       callback && callback(e.data);
     }
   };
 }
 
+const toAbsoluteUrl = (domain, path, relativeUrl) => {
+  if (_.last(domain) === '/' && _.first(relativeUrl) === '/') {
+    return domain + relativeUrl.substring(1);
+  } else if (_.last(domain) !== '/' && _.first(relativeUrl) !== '/') {
+    return domain + path + '/' + relativeUrl;
+  } else {
+    return domain + relativeUrl;
+  }
+};
+
 // TODO: maybe push this to a worker?
 export function clean(url, body) {
   let $body = $(body).wrapAll('<html></html>').parent();
+  let parser = document.createElement('a');
+  parser.href = url;
+  const domain = parser.protocol + '//' + parser.host;
+  const path = parser.pathname
 
   $body.find('link[rel="stylesheet"]').each((i, stylesheet) => {
-    if (stylesheet.href && ABSOLUTE_URL.test(stylesheet.href)) {
-      stylesheet.href = '';
+    if (stylesheet.href && !ABSOLUTE_URL.test(stylesheet.href)) {
+      stylesheet.href = toAbsoluteUrl(domain, path, stylesheet.href);
     }
   });
 
   $body.find('script').each((i, script) => {
-    if (script.src && ABSOLUTE_URL.test(script.src)) {
-      script.src = '';
+    if (script.src && !ABSOLUTE_URL.test(script.src)) {
+      script.src = toAbsoluteUrl(domain, path, script.src);
     }
   });
 
   $body.find('img').each((i, img) => {
-    if (img.src && ABSOLUTE_URL.test(img.src)) {
-      img.src = '';
+    if (img.src && !ABSOLUTE_URL.test(img.src)) {
+      img.src = toAbsoluteUrl(domain, path, img.src);
     }
   });
 
