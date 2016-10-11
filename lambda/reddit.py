7ad4de472a5e032bd62fdb878b8282b3e9e2d4b1
Alex Wissmann <wissmania@gmail.com>
2016-10-10 21:48:37 -0700

    standardize reddit output

--- lambda/reddit.py
+++ lambda/reddit.py
@@ -2,8 +2,18 @@ from utils import success_response
 import requests
 
 def handler(event, context):
-    return success_response(get_all_hot())
+    return success_response([standardize(post['data']) for post in get_all_hot()['data']['children']])
 
 def get_all_hot():
     return requests.get('https://www.reddit.com/r/all/hot.json').json()
 
+def standardize(post):
+    print post
+    return {
+        'id': post['id'],
+        'title': post['title'],
+        'score': post['score'],
+        'url': post['url'],
+        'numComments': post['num_comments'],
+        'comments': 'https://www.reddit.com/%s' % post['permalink']
+    }
