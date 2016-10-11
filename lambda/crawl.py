94207e0575a5f018d28fd20e90898570dc20f0a6
Alex Wissmann <wissmania@gmail.com>
2016-10-10 23:09:04 -0700

    Added link cleanup to crawling

--- lambda/crawl.py
+++ lambda/crawl.py
@@ -1,6 +1,8 @@
 import threading
 import json
 import urllib2
+from bs4 import BeautifulSoup
+from urlparse import urlparse, urljoin
 
 
 def handler(event, context):
@@ -16,15 +18,40 @@ def handler(event, context):
         'body': json.dumps(crawl(urls))
     }
 
+def isAbsoluteUrl(url):
+    return any([url.startswith(s) for s in ['//','http','javascript','data:']])
 
 def fetch_url(url, pages):
     urlHandler = urllib2.urlopen(url)
     html = urlHandler.read()
+    urlObj = urlparse(url)
+    prefix = urlObj.scheme + "://" + urlObj.netloc + urlObj.path
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    for script in soup.find_all('script'):
+        if script.get('src') != None and not isAbsoluteUrl(script.get('src')):
+            print script.get('src')
+            print script
+            script['src'] = urljoin(prefix, script.get('src'))
+            print script.get('src')
+
+    for stylesheet in soup.find_all('link[rel="stylesheet"]'):
+        if stylesheet.get('href') != None and not isAbsoluteUrl(stylesheet.get('href')):
+            print stylesheet.get('href')
+            stylehseet.href = urljoin(prefix, stylesheet.href)
+            print stylesheet.get('href')
+
+    for img in soup.find_all('img'):
+        if img.get('src') != None and not isAbsoluteUrl(img.get('src')):
+            print img.get('src')
+            img['src'] = urljoin(prefix, img.get('src'))
+            print img.get('src')
 
     pages.append({
         'originalUrl': url,
         'finalUrl': urlHandler.geturl(),
-        'body': html
+        'body': str(soup)
     })
 
 
